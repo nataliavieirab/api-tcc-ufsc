7ab5555a88763584cc1390592a9d1113e17dc5d2
nataliabortolivieira@gmail.com <nataliavieirab>
2024-03-05 22:18:08 -0300

    user scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { PrismaService } from './database/prisma.service';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
