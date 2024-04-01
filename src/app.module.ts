0ee9994f79e3277b2eade93a8959b04946c2c58d
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-04-01 09:08:35 -0300

    chore: prisma.module file; auth dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaService } from './database/prisma.service';
 import { AdminModule } from './modules/admin/admin.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, PrismaModule],
   controllers: [AppController],
   providers: [PrismaService],
 })
