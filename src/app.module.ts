cf0957875b6291eaaa013fc7d910f2ca159c6490
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-04-04 09:28:15 -0300

    chore(auth): create module, controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { PrismaService } from './database/prisma.service';
 import { AdminModule } from './modules/admin/admin.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { SessionModule } from './modules/session/session.module';
 
 @Module({
-  imports: [AdminModule, PrismaModule],
+  imports: [AdminModule, PrismaModule, AuthModule, SessionModule],
   controllers: [AppController],
   providers: [PrismaService],
 })
