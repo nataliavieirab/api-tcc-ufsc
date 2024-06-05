f82088723e04935e701d4982f6d6a32ad895e471
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-06-05 12:16:06 -0300

    pernissionService

---
+++ src/services/permissions/permissions.ts
@@ -0,0 +1,31 @@
+type roles = {
+  GENERAL_ADM: 'GENERAL_ADM';
+  FRANCH_ADM: 'FRANCH_ADM';
+  BRANCH_ADM: 'BRANCH_ADM';
+  WAITER: 'WAITER';
+  MANAGER: 'MANAGER';
+  RECEPTIONIST: 'RECEPTIONIST';
+  CASHIER: 'CASHIER';
+  DELIVERY_ATTENDANT: 'DELIVERY_ATTENDANT';
+};
+
+type actions =
+  | 'createUser'
+  | 'updateUser'
+  | 'deleteUser'
+  | 'findUserById'
+  | 'findAllUsers'
+  | 'createFranchise'
+  | 'deleteFranchise';
+
+const roles_permissions = {
+  createUser: ['GENERAL_ADM', 'FRANCH_ADM'],
+  updateUser: ['GENERAL_ADM'],
+  deleteUser: ['GENERAL_ADM'],
+  findUserById: ['GENERAL_ADM'],
+  findAllUsers: ['GENERAL_ADM'],
+  createFranchise: ['GENERAL_ADM'],
+  deleteFranchise: ['MANAGER'],
+};
+
+export { actions, roles, roles_permissions };
