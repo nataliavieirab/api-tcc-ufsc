1f8c966ee1c6f7167735c39c08bd14a2af409071
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-06-06 19:08:57 -0300

    removed AdminController content

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,15 +1,1 @@
-import { PermissionService } from 'src/services/permissions/permission.service';
-import { CurrentUser } from '../auth/decorators/current-users.decorator';
-import { HttpException, HttpStatus } from '@nestjs/common';
-import { User } from '@prisma/client';
-import { actions } from 'src/services/permissions/permissions';
-
-export class AdminController {
-  validateUserAccess(@CurrentUser() currentUser: User, action: actions) {
-    const permissionService = new PermissionService();
-    const hasAccess = permissionService.validateAccess(currentUser, action);
-    if (!hasAccess) {
-      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
-    }
-  }
-}
+export class AdminController {}
