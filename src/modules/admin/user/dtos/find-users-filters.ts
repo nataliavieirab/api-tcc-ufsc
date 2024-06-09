74635f9a63be307e2a105423dd85271137266d7b
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-06-09 09:01:47 -0300

    added admin module

---
+++ src/modules/admin/user/dtos/find-users-filters.ts
@@ -0,0 +1,27 @@
+import { UserRole } from '.prisma/client';
+import { IsOptional, IsString } from 'class-validator';
+
+export class findUsersFilters {
+  @IsOptional()
+  @IsString()
+  readonly name?: string;
+
+  @IsOptional()
+  @IsString()
+  readonly last_name?: string;
+
+  @IsOptional()
+  @IsString()
+  readonly cpf?: string;
+
+  @IsOptional()
+  @IsString()
+  readonly email: string;
+
+  @IsOptional()
+  @IsString()
+  readonly user_name?: string;
+
+  @IsOptional()
+  readonly role?: UserRole;
+}
