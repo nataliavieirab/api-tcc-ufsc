c5d99e54a3061c51f78db85beebab740c750b875
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-06-09 09:02:01 -0300

    added franchise module

---
+++ src/modules/franchise/franchise.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { UserRepository } from 'src/repositories/user-repository';
+import { PrismaUsersRepository } from 'src/repositories/prisma/prisma-user-repository';
+import { PrismaModule } from 'src/infra/database/prisma/prisma.module';
+import { PrismaService } from 'src/infra/database/prisma/prisma.service';
+import { UsersController } from './user/users.controller';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [UsersController],
+  providers: [
+    PrismaService,
+    {
+      provide: UserRepository,
+      useClass: PrismaUsersRepository,
+    },
+  ],
+})
+export class FranchiseModule {}
