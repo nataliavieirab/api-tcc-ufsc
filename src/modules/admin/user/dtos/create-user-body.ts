02a616a31b5f5bf6f2e52f204238126116d05d10
nataliabortolivieira@gmail.com <nataliavieirab>
2024-06-09 09:01:47 -0300

    added admin module

---
+++ src/modules/admin/user/dtos/create-user-body.ts
@@ -0,0 +1,54 @@
+import { UserRole } from '.prisma/client';
+import {
+  IsDateString,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+import { HasValidValue } from 'src/validators/has-valid-value.validator';
+
+const modulePermittedRoles: UserRole[] = ['GENERAL_ADM', 'ORGANIZATION_ADM'];
+export class CreateUserBody {
+  @IsString()
+  @Length(2, 30)
+  @IsNotEmpty({ message: 'The user name should not be empty.' })
+  readonly name: string;
+
+  @IsString()
+  @Length(5, 100)
+  @IsNotEmpty({ message: 'The user last name should not be empty.' })
+  readonly last_name: string;
+
+  @IsDateString()
+  @IsNotEmpty({ message: 'The user birth date should not be empty.' })
+  readonly birth_date: Date;
+
+  @IsString()
+  @IsNotEmpty({ message: 'The user cpf should not be empty.' })
+  readonly cpf: string;
+
+  @IsEmail()
+  @IsNotEmpty({ message: 'The user email should not be empty.' })
+  readonly email: string;
+
+  @IsString()
+  @Length(8, 24)
+  @IsNotEmpty({ message: 'The user password should not be empty.' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  readonly password: string;
+
+  @IsString()
+  @Length(5, 50)
+  @IsNotEmpty({ message: 'The user user_name should not be empty.' })
+  readonly user_name: string;
+
+  @IsNotEmpty({ message: 'The user role should not be empty.' })
+  @HasValidValue(modulePermittedRoles, {
+    message: 'Invalid role.',
+  })
+  readonly role: UserRole;
+}
---
+++ src/modules/admin/user/users.controller.ts
@@ -0,0 +1,116 @@
+import { User } from '@prisma/client';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
+import { UserRepository } from '../../../repositories/user-repository';
+import { actions } from 'src/services/permissions/permissions';
+import { CreateUserBody } from './dtos/create-user-body';
+import { UpdateUserBody } from './dtos/update-user-body';
+import { findUsersFilters } from './dtos/find-users-filters';
+import { AccessValidator } from 'src/modules/auth/decorators/access-validator.decorator';
+@Controller('admin/users')
+export class UsersController {
+  private validateAccess: (action: actions) => void;
+
+  constructor(private userRepository: UserRepository) {
+    this.validateAccess = AccessValidator;
+  }
+
+  @Get()
+  async findAll(@Body() body: findUsersFilters): Promise<User[]> {
+    this.validateAccess('findAllUsers');
+
+    return this.userRepository.findAll(body);
+  }
+
+  @Get('/:id')
+  async findById(@Param('id') id: string, @Res() res: any) {
+    this.validateAccess('findUserById');
+
+    const user = this.userRepository.findById(id);
+
+    const status = user ? 200 : 404;
+    res.status(status).send(user);
+  }
+
+  @Post()
+  async create(@Body() body: CreateUserBody, @Res() res: any) {
+    this.validateAccess('createUser');
+
+    const {
+      name,
+      last_name,
+      birth_date,
+      cpf,
+      email,
+      password,
+      user_name,
+      role,
+    } = body;
+
+    const user = await this.userRepository.create(
+      name,
+      last_name,
+      birth_date,
+      cpf,
+      email,
+      password,
+      user_name,
+      role,
+    );
+
+    res.status(201).send({ ...user, password: undefined });
+  }
+
+  @Put('/:id')
+  async update(
+    @Body() body: UpdateUserBody,
+    @Param('id') id: string,
+    @Res() res: any,
+  ) {
+    this.validateAccess('updateUser');
+
+    const {
+      name,
+      last_name,
+      birth_date,
+      cpf,
+      email,
+      password,
+      user_name,
+      role,
+    } = body;
+
+    const user = await this.userRepository.update(
+      id,
+      name,
+      last_name,
+      birth_date,
+      cpf,
+      email,
+      password,
+      user_name,
+      role,
+    );
+
+    const status = user ? 200 : 404;
+    res.status(status).send({ ...user, password: undefined });
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id') id: string, @Res() res: any) {
+    this.validateAccess('deleteUser');
+
+    const user = await this.userRepository.delete(id);
+
+    const status = user ? 204 : 404;
+    res.status(status).send();
+  }
+}
