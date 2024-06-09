fcc67e7e7aa58d18a7f9f18c016eb154425f995a
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-06-09 09:03:01 -0300

    app controller and app module changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { PrismaService } from './infra/database/prisma/prisma.service';
 import { PrismaModule } from './infra/database/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { FranchiseController } from './modules/organization/franchise/franchise.controller';
 import { OrganizationModule } from './modules/organization/organization.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UserRepository } from './repositories/user-repository';
@@ -12,24 +11,24 @@ import { FranchiseRepository } from './repositories/franchise-repository';
 import { PrismaFranchiseRepository } from './repositories/prisma/prisma-franchise-repository';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth-.guard';
-import { UsersController } from './modules/organization/user/users.controller';
+import { AdminModule } from './modules/admin/admin.module';
+import { FranchiseModule } from './modules/franchise/franchise.module';
 
 @Module({
-  imports: [OrganizationModule, PrismaModule, AuthModule, JwtModule],
-  controllers: [AppController, UsersController, FranchiseController],
+  imports: [
+    OrganizationModule,
+    PrismaModule,
+    AuthModule,
+    JwtModule,
+    AdminModule,
+    FranchiseModule,
+  ],
+  controllers: [AppController],
   providers: [
     PrismaService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
-    },
-    {
-      provide: UserRepository,
-      useClass: PrismaUsersRepository,
-    },
-    {
-      provide: FranchiseRepository,
-      useClass: PrismaFranchiseRepository,
     },
   ],
 })
