ee423053eec70d9081ddb44de853d19ebb645f33
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-06-12 16:07:15 -0300

    Hotfix: franchises dtos

--- src/modules/organization/franchise/dtos/find-franchises-filters.ts
+++ src/modules/organization/franchise/dtos/find-franchises-filters.ts
@@ -1,27 +1,35 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class findFranchisesFilters {
   @IsString()
+  @IsOptional()
   readonly name: string;
 
   @IsString()
+  @IsOptional()
   readonly cnpj: string;
 
   @IsString()
+  @IsOptional()
   readonly city: string;
 
   @IsString()
+  @IsOptional()
   readonly state: string;
 
   @IsString()
+  @IsOptional()
   readonly zip_code: string;
 
   @IsString()
+  @IsOptional()
   readonly phone: string;
 
   @IsString()
+  @IsOptional()
   readonly email: string;
 
   @IsString()
+  @IsOptional()
   readonly adm_id: string;
 }
--- src/modules/organization/franchise/dtos/update-franchise-body.ts
+++ src/modules/organization/franchise/dtos/update-franchise-body.ts
@@ -1,63 +1,63 @@
-import { IsInt, IsNotEmpty, IsString, Length } from 'class-validator';
+import { IsInt, IsOptional, IsString, Length } from 'class-validator';
 
 export class UpdateFranchiseBody {
   @IsString()
   @Length(2, 50)
-  @IsNotEmpty({ message: 'The franchise name should not be empty.' })
+  @IsOptional()
   readonly name: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'The franchise cnpj should not be empty.' })
+  @IsOptional()
   readonly cnpj: string;
 
   @IsString()
   @Length(5, 100)
-  @IsNotEmpty({ message: 'The franchise address should not be empty.' })
+  @IsOptional()
   readonly address: string;
 
   @IsInt()
-  @IsNotEmpty({ message: 'The franchise number should not be empty.' })
+  @IsOptional()
   readonly number: number;
 
   @IsString()
-  @IsNotEmpty({ message: 'The franchise complement should not be empty.' })
+  @IsOptional()
   readonly complement: string;
 
   @IsString()
   @Length(8, 50)
-  @IsNotEmpty({ message: 'The franchise neighborhood should not be empty.' })
+  @IsOptional()
   readonly neighborhood: string;
 
   @IsString()
   @Length(5, 50)
-  @IsNotEmpty({ message: 'The franchise city should not be empty.' })
+  @IsOptional()
   readonly city: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'The franchise state should not be empty.' })
+  @IsOptional()
   readonly state: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'The franchise country should not be empty.' })
+  @IsOptional()
   readonly country: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'The franchise zip_code should not be empty.' })
+  @IsOptional()
   readonly zip_code: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'The franchise phone should not be empty.' })
+  @IsOptional()
   readonly phone: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'The franchise email should not be empty.' })
+  @IsOptional()
   readonly email: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'The franchise adm_id should not be empty.' })
+  @IsOptional()
   readonly adm_id: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'The organization_id should not be empty.' })
+  @IsOptional()
   readonly organization_id: string;
 }
