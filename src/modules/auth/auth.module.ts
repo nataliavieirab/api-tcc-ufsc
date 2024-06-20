3e4f032d0a93ad061b00983f428990b7f47b656b
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-06-20 20:19:36 -0300

    chor: Refactor UserController to use UerService and UserService to use UserRepository

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthService } from 'src/services/auth.service';
+import { AuthService } from 'src/services/domains/auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { UserRepository } from 'src/repositories/user-repository';
 import { PrismaUsersRepository } from 'src/repositories/prisma/prisma-user-repository';
