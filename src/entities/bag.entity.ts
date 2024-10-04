aadafefdbf7670d3ad50e29a865c5823479230ee
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-04 20:16:39 -0300

    Order, CashRegister and Bag Entity's Adjust

--- src/entities/bag.entity.ts
+++ src/entities/bag.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToOne } from 'typeorm';
 import { DefaultEntity } from './default-entity';
 import { AddOn } from './add-on.entity';
 import { Customer } from './customer.entity';
+import { Order } from './order.entity';
 
 enum BagStatus {
   OPENED = 'opened',
@@ -18,4 +19,7 @@ export class Bag extends DefaultEntity {
 
   @Column({ type: 'enum', enum: BagStatus })
   status: BagStatus;
+
+  @OneToOne(() => Order, (order) => order.bag)
+  order: Order;
 }
