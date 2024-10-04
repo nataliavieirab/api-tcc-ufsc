45360c4145a3bcf409c9a73608c69115306f211f
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-04 19:41:26 -0300

    cashRegister and Company Entity's Adjust

--- src/entities/company.entity.ts
+++ src/entities/company.entity.ts
@@ -1,12 +1,16 @@
-import { Column, Entity, OneToOne } from 'typeorm';
+import { Column, Entity, OneToMany, OneToOne } from 'typeorm';
 import { DefaultEntity } from './default-entity';
+import { CashRegister } from './cash-register.entity';
 import { CompanyAddress } from './company-address.entity';
 
 @Entity()
 export class Company extends DefaultEntity {
-  @Column()
-  name: string;
+  @OneToMany(() => CashRegister, (cashRegister) => cashRegister.company)
+  cashRegister: CashRegister[];
 
   @OneToOne(() => CompanyAddress, (address) => address.company)
   address?: CompanyAddress;
+
+  @Column()
+  name: string;
 }
