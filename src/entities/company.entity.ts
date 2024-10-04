c9bdb7ce3d58ab5dbb2421794981fd01588b1812
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-04 19:35:02 -0300

    company  address

--- src/entities/company.entity.ts
+++ src/entities/company.entity.ts
@@ -1,8 +1,12 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToOne } from 'typeorm';
 import { DefaultEntity } from './default-entity';
+import { CompanyAddress } from './company-address.entity';
 
 @Entity()
 export class Company extends DefaultEntity {
   @Column()
   name: string;
+
+  @OneToOne(() => CompanyAddress, (address) => address.company)
+  address?: CompanyAddress;
 }
