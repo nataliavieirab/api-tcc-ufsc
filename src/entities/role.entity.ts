8c06721aba8774b2a70c1dac078eaa1f03806787
nataliabortolivieira@gmail.com <nataliavieirab>
2024-10-04 19:31:35 -0300

    rolepermission entity adjust

--- src/entities/role.entity.ts
+++ src/entities/role.entity.ts
@@ -1,11 +1,15 @@
 import { Column, Entity, OneToMany } from 'typeorm';
 import { DefaultEntity } from './default-entity';
 import { UserRole } from './user-role.entity';
+import { RolePermission } from './role-permission.entity';
 
 @Entity()
 export class Role extends DefaultEntity {
   @OneToMany(() => UserRole, (user) => user.role)
   users: UserRole[];
+
+  @OneToMany(() => RolePermission, (permission) => permission.role)
+  permissions: RolePermission[];
 
   @Column()
   name: string;
