1a7337138c1311124d63429320f4af897727b234
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-04 20:01:05 -0300

    product add on

--- src/entities/add-on.entity.ts
+++ src/entities/add-on.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 import { DefaultEntity } from './default-entity';
 import { Company } from './company.entity';
+import { ProductAddOn } from './product-add-on.entity';
 
 @Entity()
 export class AddOn extends DefaultEntity {
@@ -9,4 +10,7 @@ export class AddOn extends DefaultEntity {
 
   @Column()
   name: string;
+
+  @OneToMany(() => ProductAddOn, (productAdd) => productAdd.addOn)
+  productAddOns: ProductAddOn[];
 }
