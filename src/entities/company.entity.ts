72c052beae04c480f8755c9404ca5f09f75f3cc6
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-04 19:44:00 -0300

    payment type

--- src/entities/company.entity.ts
+++ src/entities/company.entity.ts
@@ -2,6 +2,7 @@ import { Column, Entity, OneToMany, OneToOne } from 'typeorm';
 import { DefaultEntity } from './default-entity';
 import { CashRegister } from './cash-register.entity';
 import { CompanyAddress } from './company-address.entity';
+import { PaymentType } from './payment_type.entity';
 
 @Entity()
 export class Company extends DefaultEntity {
@@ -13,4 +14,7 @@ export class Company extends DefaultEntity {
 
   @Column()
   name: string;
+
+  @OneToMany(() => PaymentType, (address) => address.company)
+  paymentTypes: PaymentType[];
 }
