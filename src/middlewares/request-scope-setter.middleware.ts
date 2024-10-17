eb79f58372ab97f04d6121b1b25839ab44fc1d9f
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-17 09:50:56 -0300

    Merge branch 'main' of https://github.com/nataliavieirab/FranchEasyAPI into main

--- src/middlewares/request-scope-setter.middleware.ts
+++ src/middlewares/request-scope-setter.middleware.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction } from 'express';
 import { CurrentRequestService } from 'src/services/application/current-request.service';
+import { TenantService } from 'src/services/application/tenant.service';
 import { DataSource } from 'typeorm';
 
 @Injectable()
@@ -8,6 +9,7 @@ export class RequestScopeSetterMiddleware implements NestMiddleware {
   constructor(
     @Inject('DataSource') public dataSource: DataSource,
     private readonly currentRequestService: CurrentRequestService,
+    private readonly tenantService: TenantService,
   ) {}
 
   async use(req: any, res: any, next: NextFunction) {
@@ -15,6 +17,10 @@ export class RequestScopeSetterMiddleware implements NestMiddleware {
       this.currentRequestService.setCurrentQueryRunner(
         this.dataSource.createQueryRunner(),
       );
+
+      const orgId = req.headers['X-API-TOKEN'];
+      if (orgId) this.tenantService.switchTenant(orgId);
+
       next();
     });
   }
