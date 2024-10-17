22d6a775064c3525707ed7d2fd33c6765f4eb495
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-17 09:23:20 -0300

    order management

---
+++ src/modules/store/order/dtos/create-order-body.ts
@@ -0,0 +1,11 @@
+import { Bag } from 'src/entities/bag.entity';
+import { CashRegister } from 'src/entities/cash-register.entity';
+import { Payment } from 'src/entities/payment.entity';
+
+export class CreateOrderBody {
+  bag: Bag;
+
+  cashRegister: CashRegister;
+
+  payments: Payment[];
+}
