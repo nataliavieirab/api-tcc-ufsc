bbc1025e55c5fc60f84afa20f4ff2bafcee54b0d
nataliabortolivieira@gmail.com <nataliavieirab>
2024-10-17 09:56:03 -0300

    company creation adjust

--- src/modules/organization/company/dtos/find-company-filters.ts
+++ src/modules/organization/company/dtos/find-company-filters.ts
@@ -1,35 +1,11 @@
 import { IsOptional, IsString } from 'class-validator';
 
-export class findCompanyFilters {
+export class FindCompanyFilters {
   @IsString()
   @IsOptional()
   readonly name: string;
 
   @IsString()
   @IsOptional()
-  readonly cnpj: string;
-
-  @IsString()
-  @IsOptional()
-  readonly city: string;
-
-  @IsString()
-  @IsOptional()
-  readonly state: string;
-
-  @IsString()
-  @IsOptional()
-  readonly zip_code: string;
-
-  @IsString()
-  @IsOptional()
-  readonly phone: string;
-
-  @IsString()
-  @IsOptional()
-  readonly email: string;
-
-  @IsString()
-  @IsOptional()
-  readonly manager_id: string;
+  readonly like_name: string;
 }
