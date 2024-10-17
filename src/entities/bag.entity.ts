1cff5d7efa1096de56dd5adb395c5836d305cc06
nataliabortolivieira@gmail.com <nataliavieirab>
2024-10-17 09:05:44 -0300

    customer register

--- src/entities/bag.entity.ts
+++ src/entities/bag.entity.ts
@@ -1,6 +1,5 @@
 import { Column, Entity, ManyToOne, OneToMany, OneToOne } from 'typeorm';
 import { DefaultEntity } from './default-entity';
-import { AddOn } from './add-on.entity';
 import { Customer } from './customer.entity';
 import { Order } from './order.entity';
 import { BagItem } from './bag-item.entity';
@@ -15,10 +14,7 @@ export class Bag extends DefaultEntity {
   @ManyToOne(() => Customer, (customer) => customer.bags)
   customer: Customer;
 
-  @ManyToOne(() => AddOn, (addOn) => addOn.productAddOns)
-  addOn: AddOn;
-
-  @Column({ type: 'enum', enum: BagStatus })
+  @Column({ type: 'enum', enum: BagStatus, default: BagStatus.OPENED })
   status: BagStatus;
 
   @OneToOne(() => Order, (order) => order.bag)
