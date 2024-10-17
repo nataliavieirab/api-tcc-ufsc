54d19486ae15506ff4a3ed12303a5c3d3472e476
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-17 09:23:08 -0300

    neighborhood and deliveryNeighborhood entity properties adjust

--- src/entities/neighborhood.entity.ts
+++ src/entities/neighborhood.entity.ts
@@ -1,13 +1,9 @@
-import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { DefaultEntity } from './default-entity';
-import { City } from './city.entity';
 import { DeliveryNeighborhood } from './delivery-neighborhood.entity';
 
 @Entity()
 export class Neighborhood extends DefaultEntity {
-  @ManyToOne(() => City, (city) => city.neighborhoods)
-  city: City;
-
   @Column()
   name: string;
 
