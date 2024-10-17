75a57babbc72e30dcbe3d5ad5bf37a7356ea6e44
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-17 14:01:42 -0300

    Merge branch 'main' of https://github.com/nataliavieirab/FranchEasyAPI into main

--- src/services/domains/customer.service.ts
+++ src/services/domains/customer.service.ts
@@ -5,12 +5,21 @@ import { CustomerRepository } from 'src/repositories/customer.repository';
 import { EntityDefaultService } from './entity-default.service';
 import { Customer } from 'src/entities/customer.entity';
 import { BagRepository } from 'src/repositories/bag.repository';
+import { AddressRepository } from 'src/repositories/address.repository';
+import { Address } from 'src/entities/address.entity';
+import { CustomerAddressRepository } from 'src/repositories/customer-address.repository';
+import { CurrentRequestService } from '../application/current-request.service';
+import { CustomerAddress } from 'src/entities/customer-address.entity';
+import { EntityPagination } from 'src/utils/entity-pagination.type';
 
 @Injectable()
 export class CustomerService extends EntityDefaultService<Customer> {
   constructor(
     customerRepository: CustomerRepository,
     private bagRepository: BagRepository,
+    private addressRepository: AddressRepository,
+    private customerAddressRepository: CustomerAddressRepository,
+    private currentRequestService: CurrentRequestService,
   ) {
     super(customerRepository);
   }
@@ -44,5 +53,54 @@ export class CustomerService extends EntityDefaultService<Customer> {
     const createdCustomer = await this.repository.update(id, input);
 
     return createdCustomer;
+  }
+
+  async addAddress(adreessAttributes: Address): Promise<Address> {
+    const address = await this.addressRepository.create(adreessAttributes);
+
+    const customer = this.currentRequestService.getCurrentCustomer();
+
+    await this.customerAddressRepository.create({
+      customer,
+      address,
+    });
+
+    return address;
+  }
+
+  async updateAddress(
+    addressId: string,
+    adreessAttributes: Address,
+  ): Promise<Address> {
+    const address = await this.addressRepository.update(
+      addressId,
+      adreessAttributes,
+    );
+
+    return address;
+  }
+
+  async removeAddress(addressId: string): Promise<void> {
+    const customerAddress = await this.customerAddressRepository.findOne({
+      conditions: {
+        addressId,
+      },
+    });
+
+    if (!customerAddress) return;
+
+    await this.customerAddressRepository.delete(customerAddress.id);
+  }
+
+  async getAddresses(): Promise<EntityPagination<CustomerAddress>> {
+    const customer = this.currentRequestService.getCurrentCustomer();
+
+    const customerAddresses = await this.customerAddressRepository.where({
+      conditions: {
+        customerId: customer.id,
+      },
+    });
+
+    return customerAddresses;
   }
 }
