ecbaff7f9e695b3b0d06a07c26d3bc3c9f614830
nataliabortolivieira@gmail.com <nataliavieirab>
2024-10-17 09:01:24 -0300

    adjust delivery settings service

--- src/services/domains/delivery-settings.service.ts
+++ src/services/domains/delivery-settings.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { DeliverySettingRepository } from 'src/repositories/delivery-setting.repository';
 import { EntityDefaultService } from './entity-default.service';
-import { DeliverySetting } from 'src/entities/delivery-setting.entity';
+import { DeliverySettings } from 'src/entities/delivery-settings.entity';
 
 @Injectable()
-export class DeliverySettingService extends EntityDefaultService<DeliverySetting> {
+export class DeliverySettingsService extends EntityDefaultService<DeliverySettings> {
   constructor(deliverySettingRepository: DeliverySettingRepository) {
     super(deliverySettingRepository);
   }
