ec6ed0acb0d7225349441b30a1d3e6c927e3f244
nataliabortolivieira@gmail.com <nataliavieirab>
2024-10-17 09:07:23 -0300

    deliverySettings management

---
+++ src/modules/store/delivery_settings/dtos/find-delivery-settings-filters.ts
@@ -0,0 +1,12 @@
+import { Company } from 'src/entities/company.entity';
+import { DeliveryStatus } from 'src/entities/delivery-settings.entity';
+
+export class FindDeliverySettingsFilters {
+  company: Company;
+
+  start_hour: string;
+
+  end_hour: string;
+
+  type: DeliveryStatus;
+}
