a47405282ba0904f4c2e5302d5b0f08f371b6c05
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-21 09:35:26 -0300

    change company to store

--- src/modules/customer/store/stores.controller.ts
+++ src/modules/customer/store/stores.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Param, Res } from '@nestjs/common';
 import { DefaultController } from 'src/modules/default.controller';
-import { CompanyService } from 'src/services/domains/company.service';
+import { StoreService } from 'src/services/domains/store.service';
 import { FindCategoriesFilters } from './dtos/find-categories-filters';
 import { Response } from 'express';
 import { CategoryService } from 'src/services/domains/category.service';
@@ -12,13 +12,13 @@ import { PaymentTypeService } from 'src/services/domains/payment-type.service';
 import { EntityPagination } from 'src/utils/entity-pagination.type';
 import { Category } from 'src/entities/category.entity';
 import { Product } from 'src/entities/product.entity';
-import { Company } from 'src/entities/company.entity';
+import { Store } from 'src/entities/store.entity';
 import { PaymentType } from 'src/entities/payment-type.entity';
 
 @Controller('customer/stores')
 export class StoresController extends DefaultController {
   constructor(
-    private storeService: CompanyService,
+    private storeService: StoreService,
     private categoryService: CategoryService,
     private productService: ProductService,
     private paymentTypeService: PaymentTypeService,
@@ -26,29 +26,29 @@ export class StoresController extends DefaultController {
     super();
   }
 
-  @Get('/:companyId/delivery-fees/:neighborhoodCode')
+  @Get('/:storeId/delivery-fees/:neighborhoodCode')
   async getDeliveryFee(
-    @Param('companyId') companyId: string,
+    @Param('storeId') storeId: string,
     @Param('neighborhoodCode') neighborhoodCode: number,
   ): Promise<number> {
     this.validateAccess('findDeliveryFee');
-    return this.storeService.getDeliveryFee(companyId, neighborhoodCode);
+    return this.storeService.getDeliveryFee(storeId, neighborhoodCode);
   }
 
-  @Get('/:companyId/categories')
+  @Get('/:storeId/categories')
   async findAllCategories(
     @Body() body: FindCategoriesFilters,
-    @Param('companyId') companyId: string,
+    @Param('storeId') storeId: string,
   ): Promise<EntityPagination<Category>> {
     this.validateAccess('findAllCategories');
 
-    return this.categoryService.findAll({ ...body, companyId });
+    return this.categoryService.findAll({ ...body, storeId });
   }
 
-  @Get('/:companyId/products')
+  @Get('/:storeId/products')
   async findAllProducts(
     @Body() body: FindProductsFilters,
-    @Param('companyId') companyId: string,
+    @Param('storeId') storeId: string,
   ): Promise<EntityPagination<Product>> {
     this.validateAccess('findAllProducts');
     return this.productService.findByCategory(body.categoryId, body);
@@ -57,7 +57,7 @@ export class StoresController extends DefaultController {
   @Get()
   async findAllStores(
     @Body() body: FindStoresFilters,
-  ): Promise<EntityPagination<Company>> {
+  ): Promise<EntityPagination<Store>> {
     this.validateAccess('findAllStores');
     return this.storeService.findAll(body);
   }
