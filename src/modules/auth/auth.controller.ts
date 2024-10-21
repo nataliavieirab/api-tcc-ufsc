1a72ea6a55fbe0e9309c447e16e32b984ab19700
Natalia Vieira <nataliabortolivieira@gmail.com>
2024-10-21 09:47:46 -0300

    customer auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,15 +5,20 @@ import {
   Post,
   UseGuards,
   Request,
+  Get,
 } from '@nestjs/common';
 import { AuthService } from 'src/services/domains/auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
 import { IsPublic } from './decorators/is-public.decorator';
+import { CurrentRequestService } from 'src/services/application/current-request.service';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private currentRequestService: CurrentRequestService,
+  ) {}
 
   @IsPublic()
   @Post('login')
@@ -21,5 +26,13 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   async login(@Request() req: AuthRequest) {
     return this.authService.login(req.user, req.orgId);
+  }
+
+  @Get('me')
+  async getMe() {
+    return (
+      this.currentRequestService.getCurrentUser() ||
+      this.currentRequestService.getCurrentCustomer()
+    );
   }
 }
